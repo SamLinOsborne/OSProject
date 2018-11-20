357a8ee43a02658ba2e3fd9881324b4ac730acef
Sam <sam-osborne@hotmail.com>
2018-11-20 12:47:00 -0600

    added bitmap and OpenMP

--- Desktop/mandlebrot.cpp
+++ Desktop/mandlebrot.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <complex>
+#include <omp.h>
+#include "bitmap_image.hpp"
 
 using namespace std;
 
-const int width = 800;
-const int height = 800;
+const int width = 900;
+const int height = 900;
 float MinRe = -2.0;
 float MaxRe = 1;
 float MinIm = -1;
 float MaxIm = 1;
 
+bitmap_image img( width, height );
+
 int mandlebrot( int x, int y, int m);
 
 int main(int argc, char *argv[])
 {
-    if( argc != 3)
+    int test_threads;
+    int max_it =  atoi(argv[1]);
+    if( argc != 2)
     {
         cout<< argc <<endl;
-        cout<< "use arguments iterations, filename" <<endl;
+        cout<< "use arguments iterations" <<endl;
         return -1;
     }
-    ofstream image(string(argv[2]) + ".ppm");
-    if( image.is_open() )
-    {
-        image << "P3\n" << width << " " << height << "\n 255" <<endl;
+    omp_set_num_threads(10);
+    #pragma omp parallel \
+    shared (img, max_it, MinRe, MaxRe, MinIm, MaxIm) \
 
-        for( int i = 0; i < width; i++)
-        {
-            for( int j = 0; j < height; j++ )
-            {
-                image<< mandlebrot( i, j, atoi(argv[1]) ) << ' ' << 0 << ' ' << 0 << endl;
-            }
-            if(!(i%30))
-                cout<< 100 * float(i)/width << "%" <<endl;
-        }
-        image.close();
-    }
-    else
+    #pragma omp for
+    for( int i = 0; i < width; i++)
     {
-        cout<< "Couldn't open image" <<endl;
+    test_threads = omp_get_num_threads();
+        for( int j = 0; j < height; j++ )
+        {
+            img.set_pixel( i, j, mandlebrot( i, j, max_it), 0, 0 );
+        }
+        if(!(i%30))
+            cout<< 100 * float(i)/width << "%" <<endl;
     }
+
+    img.save_image("Test.bmp");
+    cout<< "Used " <<test_threads << " threads" <<endl;
     return 0;
 }
 
