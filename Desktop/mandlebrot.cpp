013006d20505c0f926bee852a791571943aa1fd4
Sam <sam-osborne@hotmail.com>
2018-11-26 15:41:14 -0600

    Changed coloring scheme

--- Desktop/mandlebrot.cpp
+++ Desktop/mandlebrot.cpp
@@ -2,9 +2,19 @@
 #include <fstream>
 #include <complex>
 #include <omp.h>
+#include <tuple>
 #include "bitmap_image.hpp"
 
 using namespace std;
+
+const tuple<char,char,char> colors[5] = 
+{
+    make_tuple(230, 57, 70),
+    make_tuple(241, 250, 238), 
+    make_tuple(168, 218, 220), 
+    make_tuple(69, 123, 157),
+    make_tuple(29, 53, 87),
+};
 
 const int width = 900;
 const int height = 900;
@@ -15,7 +25,7 @@ float MaxIm = 1;
 
 bitmap_image img( width, height );
 
-int mandlebrot( int x, int y, int m);
+tuple<char,char,char> mandlebrot( int x, int y, int m);
 
 int main(int argc, char *argv[])
 {
@@ -27,6 +37,8 @@ int main(int argc, char *argv[])
         cout<< "use arguments iterations" <<endl;
         return -1;
     }
+    bitmap_image img(width, height);
+
     omp_set_num_threads(10);
     #pragma omp parallel \
     shared (img, max_it, MinRe, MaxRe, MinIm, MaxIm) \
@@ -37,7 +49,8 @@ int main(int argc, char *argv[])
     test_threads = omp_get_num_threads();
         for( int j = 0; j < height; j++ )
         {
-            img.set_pixel( i, j, mandlebrot( i, j, max_it), 0, 0 );
+            tuple<char,char,char> x = mandlebrot( i, j, max_it);
+            img.set_pixel( i, j, get<0>(x), get<1>(x), get<2>(x));
         }
         if(!(i%30))
             cout<< 100 * float(i)/width << "%" <<endl;
@@ -48,7 +61,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int mandlebrot( int x, int y, int max_it )
+tuple<char,char,char> mandlebrot( int x, int y, int max_it )
 {
     complex<float> point(
                         x*( (MaxRe-MinRe)/width ) + MinRe ,
@@ -61,7 +74,7 @@ int mandlebrot( int x, int y, int max_it )
         iterations++;
     }
     if( iterations < max_it ) 
-        return 255 * iterations/max_it;
+        return colors[iterations % 5];
     else
-        return 0;
+        return make_tuple(0,0,0);
 }
